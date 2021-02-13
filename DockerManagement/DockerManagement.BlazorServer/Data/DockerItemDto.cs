d81ddde1a7b38e9e53a8c88f4d7bcb11a3eae894
Mrmantou <albert_nchu@163.com>
2021-02-13 14:17:02 +0800

    add blazor server client

---
+++ DockerManagement/DockerManagement.BlazorServer/Data/DockerItemDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerManagement.BlazorServer.Data
+{
+    public class ContainerItemDto
+    {
+        public string short_id { get; set; }
+        public string name { get; set; }
+        public string status { get; set; }
+    }
+
+    public class ImageItemDto
+    {
+        public string id { get; set; }
+        public string short_id { get; set; }
+        public string[] tags { get; set; }
+    }
+}
---
+++ DockerManagement/DockerManagement.BlazorServer/Services/DockerService.cs
@@ -0,0 +1,43 @@
+﻿using DockerManagement.BlazorServer.Data;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace DockerManagement.BlazorServer.Services
+{
+    public class DockerService
+    {
+        private readonly HttpClient client;
+        private readonly string baseUrl;
+
+        public DockerService(IConfiguration configuration)
+        {
+            client = new HttpClient();
+            baseUrl = configuration["BaseServerUrl"];
+        }
+
+        public async Task<ContainerItemDto[]> GetContainers()
+        {
+            return await client.GetFromJsonAsync<ContainerItemDto[]>($"{baseUrl}docker/containers");
+        }
+
+        public async Task<string> StopContainer(string idOrName)
+        {
+            return await client.GetStringAsync($"{baseUrl}docker/containers/stop/{idOrName}");
+        }
+
+        public async Task<ImageItemDto[]> GetImages()
+        {
+            return await client.GetFromJsonAsync<ImageItemDto[]>($"{baseUrl}docker/images");
+        }
+
+        public async Task<string> RemoveImageById(string idOrName)
+        {
+            return await client.GetStringAsync($"{baseUrl}docker/images/remove/{idOrName}");
+        }
+    }
+}
